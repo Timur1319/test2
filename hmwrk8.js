b2a697e5ccf6a56afdcaf32a87afa35a5c9dba35
Timur1319 <timur09mertsoy09@gmail.com>
2025-01-19 13:54:58 +0300

    test

---
+++ hmwrk8.js
@@ -0,0 +1,35 @@
+let name = "Timur";
+let age = 10;
+let likesProgramming = true;
+
+console.log(name);
+console.log(age);
+console.log(likesProgramming);
+javascript;
+
+let city = "TimLandia";
+let population = 300000;
+let isCapital = true;
+
+console.log(typeof city);
+console.log(typeof population);
+console.log(typeof isCapital);
+
+let a = 10;
+let b = 5;
+
+console.log("сума a + b:", a + b);
+console.log("різниця a - b:", a - b);
+console.log("добуток a * b:", a * b);
+а;
+console.log("частка a / b:", a / b);
+
+javascript;
+
+let user = {
+  name: "Timur",
+  age: 10,
+  city: "Antalya",
+};
+
+console.log(user.name);
